7a0242c9a7dfe8d2c9ec29d1085eaf75c03d3765
Nicholas Griscti Soler <nicholas.griscti.14@um.edu.mt>
2016-03-14 09:53:18 +0100

    added constructor and randoms, "cool"

--- src/main/java/com/examples/one/petstore/Puppy.java
+++ src/main/java/com/examples/one/petstore/Puppy.java
@@ -1,73 +1,78 @@
-
 package com.examples.one.petstore;
 
 public class Puppy {
+
     private int id;
     private String name;
     private String breed;
-    private boolean loadBark;
+    private boolean loadBark=false;
     private int exerciseRequiredPerDay;
-    private boolean friendlyWithChildren;
-    
-    
+    private boolean friendlyWithChildren=false;
+
+    public Puppy(int id, String name, String breed) {
+        this.id = id;
+        this.name = name;
+        this.breed = breed;
+        int loadR=(int)(Math.random()*1+1);
+        int friendR=(int)(Math.random()*1+1);
+        exerciseRequiredPerDay=(int)(Math.random()*4+1);
+        if(loadR==1){
+            loadBark=true;
+        }
+        if(friendR==1){
+            loadBark=true;
+        }
+        
+    }
     //GETTERS
-    
-    public int getID(){
+
+    public int getID() {
         return this.id;
     }
-    
-     public String getName(){
+
+    public String getName() {
         return this.name;
     }
-     
-      public String getBreed(){
+
+    public String getBreed() {
         return this.breed;
     }
-      
-       public Boolean getLoadBark(){
+
+    public Boolean getLoadBark() {
         return this.loadBark;
     }
-       
-        public int getExercise(){
+
+    public int getExercise() {
         return this.exerciseRequiredPerDay;
     }
-        
-        public Boolean getFriendly(){
+
+    public Boolean getFriendly() {
         return this.friendlyWithChildren;
     }
+
     //SETTERS
-    public void setID(int id){
-        this.id=id;
-    }
-    
-    
-     public void setName(String name){
-        this.name = name;
-    }
-     
-      public void setBreed(String br){
-        this.breed = br;
-    }
-      
-       public void setLoadBark(boolean lb){
-        this.loadBark = lb;
-    }
-       
-        public void setExercise(int erpd){
-        this.exerciseRequiredPerDay = erpd;
-    }
-        
-        public void setFriendly(boolean fwc){
-        this.friendlyWithChildren = fwc;
-    }
-        
-    public void newPup() {
-        this.id = 0;
-        this.name = "labrador";
-        this.breed = "pitbul";
-        this.loadBark = true;
-        this.exerciseRequiredPerDay = 1;
-        this.friendlyWithChildren = true;
+
+    public void setID(int id) {
+        this.id = id;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBreed(String br) {
+        this.breed = br;
+    }
+
+    public void setLoadBark(boolean lb) {
+        this.loadBark = lb;
+    }
+
+    public void setExercise(int erpd) {
+        this.exerciseRequiredPerDay = erpd;
+    }
+
+    public void setFriendly(boolean fwc) {
+        this.friendlyWithChildren = fwc;
+    }
 }
